07c0550a0cb9b1e4c2439253f2ac7ae857031086
Sam Kauffman <samueljkauff@gmail.com>
2026-04-13 18:13:08 -0400

    Hero section work

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,9 +1,17 @@
 <template>
-    <div>
-        <h1 class="text-5xl font-bold">Hello, I'm Sam Kauffman</h1>
-        <h2 class="text-2xl">Computer Science | Full Stack</h2>
-        <p class="text-xl"><a href="https://www.bellarmine.edu/">Bellarmine Univeristy</a> 2026 Graduate</p>
+  <div class="flex w-full h-screen items-center gap-12">
+    <div class="flex flex-col items-center text-center">
+      <h1 class="text-5xl font-bold">Hello, I'm Sam Kauffman</h1>
+      <h2 class="text-xl mt-2">Computer Science | Full Stack</h2>
+      <p class="text-lg mt-2">
+        <a href="https://www.bellarmine.edu/">Bellarmine University</a> 2026 Graduate
+      </p>
     </div>
+
+    <div class="flex justify-center items-center">
+      <img class="max-w-sm w-full h-auto" src="../images/headshot.png" />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
