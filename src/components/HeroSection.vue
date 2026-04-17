5dd749538f7f0796f3c5e58cf5ee94b307ca96bf
Sam Kauffman <samueljkauff@gmail.com>
2026-04-17 02:18:08 -0400

    Nav bar, site rework

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="flex w-full h-screen items-center gap-12">
-    <div class="flex flex-col items-center text-center">
+  <NavigationBar/>
+  <div class="flex flex-col-2 items-center gap-12">
+    <div class="items-center">
       <h1 class="text-5xl font-bold">Hello, I'm Sam Kauffman</h1>
       <h2 class="text-xl mt-2">Computer Science | Full Stack</h2>
       <p class="text-lg mt-2">
         <a href="https://www.bellarmine.edu/">Bellarmine University</a> 2026 Graduate
       </p>
     </div>
-
     <div class="flex justify-center items-center">
       <img class="max-w-sm w-full h-auto" src="../images/headshot.png" />
     </div>
@@ -15,5 +15,17 @@
 </template>
 
 <script lang="ts">
+import NavigationBar from './NavigationBar.vue';
 
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    NavigationBar
+  }
+}
 </script>
--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,17 +1,33 @@
 <template>
-    <div>
+    <div class="flex flex-col justify-start w-15 items-center h-screen bg-[#1b4332]">
         <div>
-            
+            <i class="pi pi-bars icon"></i>
         </div>
         <div>
-
+            <i class="pi pi-code icon"></i>
         </div>
         <div>
-
+            <i class="pi pi-user icon"></i>
+        </div>
+        <div>
+            <i class="pi pi-comment icon"></i>
+        </div>
+        <div>
+            <i class="pi pi-envelope icon"></i>
         </div>
     </div>
 </template>
 
 <script lang="ts">
+import 'primeicons/primeicons.css';
+
 
 </script>
+
+<style>
+    .icon {
+        font-size: 25px;
+        margin-top: 15px;
+        color: #d8f3dc;
+    }
+</style>
