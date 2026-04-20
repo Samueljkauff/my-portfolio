1d17e97af639a13a07446f17159281b67e947eb2
Sam Kauffman <samueljkauff@gmail.com>
2026-04-20 00:27:00 -0400

    Clickable nav

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,19 +1,19 @@
 <template>
-    <div class="flex flex-col justify-start w-15 items-center h-screen bg-[#1b4332]">
+    <div @click="onExpandClick" :class="['flex flex-col justify-start h-screen bg-[#1b4332]', isOpen ? 'w-40 items-start' : 'w-15 items-center']">
         <div>
             <i class="pi pi-bars icon"></i>
         </div>
         <div>
-            <i class="pi pi-code icon"></i>
+            <i class="pi pi-code icon"> <p v-if="isOpen" class="inline"> Project</p></i>
         </div>
         <div>
-            <i class="pi pi-user icon"></i>
+            <i class="pi pi-user icon"><p v-if="isOpen" class="inline"> About</p></i>
         </div>
         <div>
-            <i class="pi pi-comment icon"></i>
+            <i class="pi pi-comment icon"><p v-if="isOpen" class="inline"> Comment</p></i>
         </div>
         <div>
-            <i class="pi pi-envelope icon"></i>
+            <i class="pi pi-envelope icon"><p v-if="isOpen" class="inline"> Message</p></i>
         </div>
     </div>
 </template>
@@ -21,6 +21,18 @@
 <script lang="ts">
 import 'primeicons/primeicons.css';
 
+export default {
+    data() {
+        return {
+            isOpen: false,
+        }
+    },
+    methods: {
+        onExpandClick() {
+            this.isOpen = !this.isOpen;
+        },
+    }
+}
 
 </script>
 
