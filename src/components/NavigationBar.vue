c4e88f4afc85e882b1a647c0374d4bdc1994852d
Sam Kauffman <samueljkauff@gmail.com>
2026-04-20 16:12:59 -0400

    Better nav styling

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,19 +1,19 @@
 <template>
-    <div @click="onExpandClick" :class="['flex flex-col justify-start h-screen bg-[#1b4332]', isOpen ? 'w-40 items-start' : 'w-15 items-center']">
+    <div :class="['flex flex-col justify-start h-screen bg-[#1b4332]', isOpen ? 'w-45 items-start' : 'w-15 items-start']">
         <div>
-            <i class="pi pi-bars icon"></i>
+            <i @click="onExpandClick" :class="isOpen ? 'pi pi-arrow-left icon justify-end' : 'pi pi-bars icon'"></i>
         </div>
         <div>
-            <i class="pi pi-code icon"> <p v-if="isOpen" class="inline"> Project</p></i>
+            <i class="pi pi-code icon"> <p v-if="isOpen" class="inline text"> Projects</p></i>
         </div>
         <div>
-            <i class="pi pi-user icon"><p v-if="isOpen" class="inline"> About</p></i>
+            <i class="pi pi-user icon"><p v-if="isOpen" class="inline text"> About</p></i>
         </div>
         <div>
-            <i class="pi pi-comment icon"><p v-if="isOpen" class="inline"> Comment</p></i>
+            <i class="pi pi-comment icon"><p v-if="isOpen" class="inline text"> Comment</p></i>
         </div>
         <div>
-            <i class="pi pi-envelope icon"><p v-if="isOpen" class="inline"> Message</p></i>
+            <i class="pi pi-envelope icon"><p v-if="isOpen" class="inline text"> Message</p></i>
         </div>
     </div>
 </template>
@@ -41,5 +41,11 @@ export default {
         font-size: 25px;
         margin-top: 15px;
         color: #d8f3dc;
+        margin-left: 15px;
+        margin-right: 4px;
+    }
+
+    .text {
+        margin-right: 2px;
     }
 </style>
